042c041123c542d106fb121a16d5261783973c98
Sampath <sampathatmuri31@gmail.com>
2023-12-24 06:14:54 -0500

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -5,11 +5,25 @@ file_paths = [
     "/var/Lizard/Policies/LizardPolicies.kql",
     "/var/Park/Tables/ParkTable.kql",
     "/var/Park/Functions/ParkFunctions.kql",
-   
     "/var/Garden/Functions/GardenFunctions.kql",
     "/var/Lizard/Tables/LizardTables.kql",
-    "/var/Lizard/Functions/LizardFunctions.kql"
-    # ... more paths
+    "/var/Lizard/Functions/LizardFunctions.kql",
+    "/var/Cat/Tables/CatTables_abc123.kql",
+    "/var/Dog/Functions/DogFunctions_xyz456.kql",
+    "/var/Bird/Tables/BirdTables_pqr789.kql",
+    "/var/Cat/Functions/CatFunctions_def321.kql",
+    "/var/Dog/Tables/DogTables_ghi987.kql",
+    "/var/Bird/Functions/BirdFunctions_jkl654.kql",
+    "/var/Apple/Tables/AppleTables_abc123.kql",
+    "/var/Orange/Functions/OrangeFunctions_xyz456.kql",
+    "/var/Banana/Tables/BananaTables_pqr789.kql",
+    "/var/Apple/Functions/AppleFunctions_def321.kql",
+    "/var/Orange/Tables/OrangeTables_ghi987.kql",
+    "/var/Banana/Functions/BananaFunctions_jkl654.kql",
+    "/var/Mango/Tables/MangoTables_mno789.kql",
+    "/var/Grapes/Functions/GrapesFunctions_xyz987.kql",
+    "/var/Pineapple/Tables/PineappleTables_qwe123.kql",
+    "/var/Watermelon/Functions/WatermelonFunctions_abc456.kql"
 ]
 
 # Define the order of subfolders within each main folder
@@ -42,6 +56,15 @@ for path in file_paths:
 # Sort paths within each folder based on the specified order
 for folder, paths in sorted_paths.items():
     sorted_paths[folder] = [p for folder_order_item in folder_order for p in paths if p[0] == folder_order_item]
+
+# Use Trandaional For loops without list comprehension
+for folder, paths in sorted_paths.items():
+    sorted_paths[folder] = []
+    for folder_order_item in folder_order:
+        for p in paths:
+            if p[0] == folder_order_item:
+                sorted_paths[folder].append(p)
+
 
 # Print the sorted paths
 for folder, paths in sorted_paths.items():
