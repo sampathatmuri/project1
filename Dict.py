3ad637db64e4d6daa3460fd8f75f8d8703200b15
Sampath <sampathatmuri31@gmail.com>
2023-12-30 06:21:38 -0500

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -7,6 +7,7 @@ from azure.kusto.data.exceptions import KustoServiceError
 # Set up logging
 logging.basicConfig(filename='kusto_script.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Retrieving necessary Azure credentials from environment variables
 cluster = os.environ.get('CLUSTER_URL')
 database = os.environ.get('DATABASE')
 azure_client_id = os.environ.get('AZURE_CLIENT_ID')
@@ -15,6 +16,7 @@ azure_tenant_id = os.environ.get('AZURE_TENANT_ID')
 
 
 def run_query(folder_path, connection):
+    # List of folders to process in a specific order
     folder_order = [
         "PreProcess",
         "Tables",
@@ -27,22 +29,30 @@ def run_query(folder_path, connection):
         "Mappings",
         "PostProcess"
     ]
+
+    # Constructing the main folder path
     home = os.path.join(os.environ.get('WORKSPACE'), folder_path)
     logging.info(f"Main Folder path: {home}")
+
+    # Processing each folder in the defined order
     for folder_name in folder_order:
         logging.info(f"Started Processing for: {folder_name}")
         read_files_in_directory(os.path.join(home, folder_name), connection)
 
 
 def read_files_in_directory(directory, connection):
+    # Checking if the directory exists
     if not os.path.exists(directory):
         logging.error(f"Directory not found: {directory}")
         return
 
     try:
+        # Iterating through files in the directory
         for file_name in os.listdir(directory):
             file_path = os.path.join(directory, file_name)
             logging.info(f"Attempting to open: {file_path}")
+            
+            # Processing KQL files
             if os.path.isfile(file_path) and file_name.endswith('.kql'):
                 logging.info(f"Started Processing for the file: {file_name}")
                 with open(file_path, 'r') as file:
@@ -60,13 +70,16 @@ def read_files_in_directory(directory, connection):
 
 
 if __name__ == "__main__":
+    # Checking if the script is provided with the required arguments
     if len(sys.argv) != 2:
         print("Usage: python script.py <folder_name>")
         sys.exit(1)
 
+    # Obtaining the folder name from command line argument
     folder_name = sys.argv[1].strip()
     logging.info(f"Received Deployment for Application: {folder_name}")
 
+    # Creating Kusto client instance and running the queries
     kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(
         cluster, azure_client_id, azure_client_secret, azure_tenant_id
     )
